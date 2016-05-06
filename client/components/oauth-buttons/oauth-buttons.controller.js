60339aaca72a633a6e5909e99647f56b8420de6d
Juan A Cortes <juan.cortes@gmail.com>
2016-05-05 22:51:48 -0400

    inital scaffolding

---
+++ client/components/oauth-buttons/oauth-buttons.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('meanTutorialApp')
+  .controller('OauthButtonsCtrl', function($window) {
+    this.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
+  });
